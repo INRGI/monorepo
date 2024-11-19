8dd856fa99af5dcef5c12913299cce7248104d88
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-11-19 13:55:36 +0100

    users lib jsdocs

--- libs/users/src/lib/hero.service.ts
+++ libs/users/src/lib/hero.service.ts
@@ -7,14 +7,30 @@ import { Hero, HeroDocument } from './shemas/hero.schema';
 export class HeroService {
   constructor(@InjectModel(Hero.name) private heroModel: Model<HeroDocument>) {}
 
+  /**
+   * Find a hero by their associated user id.
+   * @param userId The id of the user to find the hero for.
+   * @returns The hero document if found, otherwise null.
+   */
   async findByUserId(userId: Types.ObjectId): Promise<HeroDocument | null> {
     return await this.heroModel.findOne({ _id: userId }).exec();
   }
 
+  /**
+   * Finds all heroes in the database.
+   * @returns A promise that resolves with an array of HeroDocuments.
+   */
   async findAll(): Promise<HeroDocument[]> {
     return await this.heroModel.find();
   }
 
+  /**
+   * Increase the coins of the hero with the given id.
+   * @param heroId The id of the hero to increase the coins for.
+   * @param coins The amount of coins to increase.
+   * @returns A promise that resolves with the hero document after the update.
+   * @throws {NotFoundException} If the hero with the given id is not found.
+   */
   async earnCoins(heroId: Types.ObjectId, coins: number): Promise<Hero> {
     const hero = await this.findByUserId(heroId);
 
@@ -27,6 +43,14 @@ export class HeroService {
     return hero;
   }
 
+/**
+ * Decreases the coins of the hero with the specified id.
+ * @param heroId The id of the hero whose coins are to be decreased.
+ * @param coins The amount of coins to decrease.
+ * @returns A promise that resolves with the hero document after the update.
+ * @throws {NotFoundException} If the hero with the given id is not found.
+ * @throws {HttpException} If the hero does not have enough coins.
+ */
   async spendCoins(heroId: Types.ObjectId, coins: number): Promise<Hero> {
     const hero = await this.findByUserId(heroId);
     if (!hero) {
@@ -39,6 +63,14 @@ export class HeroService {
     return await hero.save();
   }
 
+
+  /**
+   * Adds the given amount of experience points to the hero with the given id.
+   * @param heroId The id of the hero to add experience points to.
+   * @param xp The amount of experience points to add.
+   * @returns A promise that resolves with the hero document after the update.
+   * @throws {NotFoundException} If the hero with the given id is not found.
+   */
   async addXp(heroId: Types.ObjectId, xp: number): Promise<Hero> {
     const hero = await this.findByUserId(heroId);
 
@@ -56,6 +88,13 @@ export class HeroService {
     return await hero.save();
   }
 
+  /**
+   * Increases the health of the hero with the given id.
+   * @param heroId The id of the hero to increase the health of.
+   * @param health The amount of health to increase.
+   * @returns A promise that resolves with the hero document after the update.
+   * @throws {NotFoundException} If the hero with the given id is not found.
+   */
   async addHealth(heroId: Types.ObjectId, health: number): Promise<Hero> {
     const hero = await this.findByUserId(heroId);
     if (!hero) {
@@ -67,6 +106,13 @@ export class HeroService {
     return await hero.save();
   }
 
+/**
+ * Increases the attack power of the hero with the given id.
+ * @param heroId The id of the hero whose attack power is to be increased.
+ * @param attack The amount of attack power to add.
+ * @returns A promise that resolves with the hero document after the update.
+ * @throws {NotFoundException} If the hero with the given id is not found.
+ */
   async addAttack(heroId: Types.ObjectId, attack: number): Promise<Hero> {
     const hero = await this.findByUserId(heroId);
 
@@ -78,6 +124,13 @@ export class HeroService {
     return await hero.save();
   }
 
+  /**
+   * Decreases the health of the hero with the given id.
+   * @param heroId The id of the hero whose health is to be decreased.
+   * @param health The amount of health to decrease.
+   * @returns A promise that resolves with the hero document after the update.
+   * @throws {NotFoundException} If the hero with the given id is not found.
+   */
   async minusHealth(heroId: Types.ObjectId, health: number): Promise<Hero> {
     const hero = await this.findByUserId(heroId);
     if (!hero) {
@@ -89,6 +142,13 @@ export class HeroService {
     return await hero.save();
   }
 
+  /**
+   * Decreases the attack power of the hero with the given id.
+   * @param heroId The id of the hero whose attack power is to be decreased.
+   * @param attack The amount of attack power to subtract.
+   * @returns A promise that resolves with the hero document after the update.
+   * @throws {NotFoundException} If the hero with the given id is not found.
+   */
   async minusAttack(heroId: Types.ObjectId, attack: number): Promise<Hero> {
     const hero = await this.findByUserId(heroId);
     if (!hero) {
@@ -100,6 +160,13 @@ export class HeroService {
     return await hero.save();
   }
 
+  /**
+   * Increases the health points of the hero with the given id.
+   * @param heroId The id of the hero whose health points are to be increased.
+   * @param hp The amount of health points to add.
+   * @returns A promise that resolves with the hero document after the update.
+   * @throws {NotFoundException} If the hero with the given id is not found.
+   */
   async addHP(heroId: Types.ObjectId, hp: number): Promise<Hero> {
     const hero = await this.findByUserId(heroId);
 
@@ -111,6 +178,13 @@ export class HeroService {
     return await hero.save();
   }
 
+  /**
+   * Decreases the health points of the hero with the given id.
+   * @param heroId The id of the hero whose health points are to be decreased.
+   * @param hp The amount of health points to subtract.
+   * @returns A promise that resolves with the hero document after the update.
+   * @throws {NotFoundException} If the hero with the given id is not found.
+   */
   async minusHP(heroId: Types.ObjectId, hp: number): Promise<Hero> {
     const hero = await this.findByUserId(heroId);
     if (!hero) {
@@ -124,6 +198,11 @@ export class HeroService {
     return await hero.save();
   }
 
+  /**
+   * Increases the level of the given hero by one, resets the experience,
+   * and increases the attack and health.
+   * @param hero The hero document to level up.
+   */
   private levelUp(hero: HeroDocument) {
     hero.level += 1;
     hero.experience = 0;
@@ -131,6 +210,11 @@ export class HeroService {
     hero.health += 10;
   }
 
+  /**
+   * Calculates the amount of experience points required to level up.
+   * @param level The current level of the hero.
+   * @returns The amount of experience points required to level up.
+   */
   private getExpToLevelUp(level: number): number {
     return 100 * level;
   }
--- libs/users/src/lib/users.service.ts
+++ libs/users/src/lib/users.service.ts
@@ -11,6 +11,12 @@ import { Hero, HeroDocument } from './shemas/hero.schema';
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>, @InjectModel(Hero.name) private heroModel: Model<HeroDocument>) {}
 
+  /**
+   * Creates a new user
+   * @param createUserDto contains the email and password of the new user
+   * @returns The newly created user
+   * @throws ConflictException if a user with the same email already exists
+   */
   async create(createUserDto: CreateUserDto): Promise<User> {
     const { email, password } = createUserDto;
 
@@ -35,12 +41,23 @@ export class UsersService {
     return await newUser.save();
   }
 
+  /**
+   * Finds a user by email
+   * @param email the email of the user to find
+   * @returns The user with the given email, or null if no such user exists
+   */
   async findOneByEmail(email: string): Promise<User | null> {
     const user = await this.userModel.findOne({ email });
     
     return user;
   }
 
+  /**
+   * Checks if a given password matches a hashed password
+   * @param plainPassword the unhashed password to check
+   * @param hashedPassword the hashed password to check against
+   * @returns true if the passwords match, false otherwise
+   */
   async validatePassword(plainPassword: string, hashedPassword: string): Promise<boolean> {
     return await bcrypt.compare(plainPassword, hashedPassword);
   }
