a46e2eb5ef29b06a987ae3505fa44824bd6b5b8f
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-12-10 15:52:44 +0100

    jsdocs

--- apps/backend/src/app/quests/controllers/quests.controller.ts
+++ apps/backend/src/app/quests/controllers/quests.controller.ts
@@ -15,26 +15,52 @@ export class QuestsController {
     constructor(private readonly questsService: QuestsService){}
 
     @Post()
+    /**
+     * Creates a new quest with the given information.
+     * @param createQuestDto The quest information.
+     * @returns The newly created quest.
+     */
     async createQuest(@Body(ValidationPipe) createQuestDto: CreateQuestDto):Promise<Quests> {
         return await this.questsService.createQuest(createQuestDto);
     }
 
     @Put()
+    /**
+     * Updates a quest with the given information.
+     * @param updateQuestDto The quest information with the fields to be updated.
+     * @returns The updated quest.
+     */
     async updateQuest(@Body(ValidationPipe) updateQuestDto: UpdateQuestDto):Promise<Quests> {
         return await this.questsService.updateQuest(updateQuestDto);
     }
 
     @Delete(':id')
+    /**
+     * Deletes a quest with the given id.
+     * @param id The id of the quest to be deleted.
+     * @returns A string indicating that the quest was deleted successfully.
+     */
     async deleteQuest(@Param('id', ParseIntPipe) id: number): Promise<string> {
         return await this.questsService.deleteQuest(id);
     }
 
     @Get(':id')
+    /**
+     * Retrieves all quests for the hero with the given id.
+     * If the hero does not have a particular quest, it is created.
+     * @param heroId The id of the hero
+     * @returns All quests for the hero
+     */
     async getAllQuest(@Param('id') heroId: string):Promise<HeroQuest[]> {
         return await this.questsService.getAllQuest(heroId);
     }
 
     @Put('status')
+    /**
+     * Updates the status of a hero's quest.
+     * @param updateStatusDto The quest status information with the fields to be updated.
+     * @returns The updated HeroQuest.
+     */
     async updateStatus(@Body(ValidationPipe) updateStatusDto: StatusUpdateDto):Promise<HeroQuest> {
         return await this.questsService.updateStatus(updateStatusDto);
     }
--- apps/backend/src/app/quests/services/quests.service.ts
+++ apps/backend/src/app/quests/services/quests.service.ts
@@ -13,26 +13,52 @@ export class QuestsService {
     this.queueEvents = new QueueEvents('quests');
   }
 
+  /**
+   * Adds a job to the quest queue to create a new quest.
+   * @param createQuestDto - The object containing the data for the new quest.
+   * @returns A promise that resolves to the newly created Quest entity.
+   */
   async createQuest(createQuestDto: CreateQuestDto) {
     const job = await this.questsQueue.add('create-quest', {createQuestDto});
     const result = await job.waitUntilFinished(this.queueEvents);
     return result;
   }
+  /**
+   * Updates a quest with the specified id.
+   * @param updateQuestDto - The object containing the updated quest data.
+   * @returns A promise that resolves to the updated Quest entity.
+   */
   async updateQuest(updateQuestDto: UpdateQuestDto) {
     const job = await this.questsQueue.add('update-quest', {updateQuestDto});
     const result = await job.waitUntilFinished(this.queueEvents);
     return result;
   }
+  /**
+   * Deletes a quest with the specified id.
+   * @param id - The id of the quest to be deleted.
+   * @returns A promise that resolves to a string indicating that the quest was deleted successfully.
+   */
   async deleteQuest(id: number) {
     const job = await this.questsQueue.add('delete-quest', {id});
     const result = await job.waitUntilFinished(this.queueEvents);
     return result;
   }
+  /**
+   * Retrieve all quests for a hero.
+   * @param heroId - the id of the hero.
+   * @returns a promise that resolves to an array of HeroQuest entities.
+   */
   async getAllQuest(heroId: string) {
     const job = await this.questsQueue.add('get-all-quests', {heroId});
     const result = await job.waitUntilFinished(this.queueEvents);
     return result;
   }
+  /**
+   * Update the status of a hero's quest.
+   * @param updateStatusDto - the object containing the id of the quest and the hero,
+   *                          and the boolean indicating whether the quest is completed.
+   * @returns a promise that resolves to the updated HeroQuest entity.
+   */
   async updateStatus(updateStatusDto: StatusUpdateDto) {
     const job = await this.questsQueue.add('update-status-hero-quest', {updateStatusDto});
     const result = await job.waitUntilFinished(this.queueEvents);
