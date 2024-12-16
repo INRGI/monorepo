005aaef003ef2e34ca45e08b0ef6d15462e6e4cd
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-12-16 22:42:59 +0100

    jsdocs

--- apps/backend/src/app/potion/potion.service.ts
+++ apps/backend/src/app/potion/potion.service.ts
@@ -12,18 +12,37 @@ export class PotionService {
     this.queueEvents = new QueueEvents('potion');
   }
 
+  /**
+   * Retrieves all potions from the potion service.
+   * 
+   * @returns A promise that resolves to an array of all potions.
+   */
   async getAllPotions() {
     const job = await this.potionQueue.add('get-all-potions', {});
     const result = await job.waitUntilFinished(this.queueEvents);
     return result;
   }
-
+  /**
+   * Creates a new potion in the potion service.
+   * 
+   * @param createPotionDto The data to create the potion with.
+   * 
+   * @returns A promise that resolves to the newly created potion.
+   */
   async createPotion(createPotionDto: CreatePotionDto) {
     const job = await this.potionQueue.add('create', createPotionDto);
-    
     const result = await job.waitUntilFinished(this.queueEvents);
     return result;
   }
+
+  /**
+   * Adds a potion to a hero's inventory.
+   * 
+   * @param potionId The ID of the potion to be added.
+   * @param heroId The ID of the hero to whom the potion will be added.
+   * 
+   * @returns A promise that resolves to the result of the job that adds the potion.
+   */
 
   async addPotionToHero(potionId: number, heroId: string) {
     const job = await this.potionQueue.add('add', {potionId, heroId});
@@ -31,6 +50,15 @@ export class PotionService {
     return result;
   }
 
+  /**
+   * Activates a potion that the hero has in their inventory.
+   * 
+   * @param activatePotionDto The data to activate the potion with.
+   * 
+   * @returns A promise that resolves to the activated potion.
+   * 
+   * A job is added to the queue to delete the potion after 1 minute.
+   */
   async activatePotion(activatePotionDto: ActivatePotionDto) {
     const job = await this.potionQueue.add('activate', activatePotionDto);
     const result = await job.waitUntilFinished(this.queueEvents);
@@ -38,6 +66,14 @@ export class PotionService {
     await this.potionQueue.add('delete-after-timeout', { heroPotionId: result.id }, { delay: 60000 });
     return result;
   }
+
+  /**
+   * Retrieves all potions associated with a specific hero.
+   * 
+   * @param heroId The unique identifier of the hero whose potions are to be retrieved.
+   * 
+   * @returns A promise that resolves to an array of potions belonging to the specified hero.
+   */
 
   async getHeroesPotions(heroId: string) {
     const job = await this.potionQueue.add('get-heroes', {heroId});
